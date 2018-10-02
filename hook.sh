ac9127693d01f59ad17e4eea03a145b47d3b81f8
Ruoxi Wang <thuwrx10@gmail.com>
2018-10-02 17:23:34 +0800

    Default max sessions per client is set to 3.

--- hook.sh
+++ hook.sh
@@ -4,6 +4,9 @@ SQLITE="/usr/bin/sqlite3"
 if [ -n $OPENVPN_DBFILE ]; then
 	OPENVPN_DBFILE="ovpn_session.db"
 fi
+if [ -n $OPENVPN_MAXCONN_PER_USER ]; then
+	OPENVPN_MAXCONN_PER_USER=3
+fi
 
 db_query(){
 	result=$($SQLITE $OPENVPN_DBFILE "$1" 2>&1)
@@ -17,4 +20,20 @@ db_query(){
 	return 0
 }
 
+create_table(){
+
+}
+
 # db_query 'insert into a123 (number) values (1);'
+case $script_type in
+	up)
+		create_table
+		;;
+	client-connect)
+		;;
+	learn-address)
+		;;
+	client-disconnect)
+		;;
+	down)
+
