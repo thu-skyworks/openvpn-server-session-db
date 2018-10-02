4a4fb11a92e9b5185f6f9f638b2fa5173f8e9938
Ruoxi Wang <thuwrx10@gmail.com>
2018-10-02 22:14:44 +0800

    F**k bash scripts. Let's use Python instead.

---
+++ hook.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+
+scriptType = os.envron.get('script_type')
+if scriptType == 'up':
+    pass
+elif scriptType == 'down':
+    pass
+elif scriptType == 'client-connect':
+    pass
+elif scriptType == 'client-disconnect':
+    pass
+elif scriptType == 'learn-address':
+    pass
